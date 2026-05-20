3b8c9771c6ea35ffe780a319e36c249ce0df7a94
Jan Janssen <janssen@mpie.de>
2026-05-20 14:44:14 -0600

    specific functions

--- workflow.py
+++ workflow.py
@@ -1,10 +1,13 @@
 def get_prod_and_div(x, y):
-    return {"prod": x * y, "div": x / y}
+    # return {"prod": x * y, "div": x / y}
+    return True, {"prod": x * y, "div": x / y}
 
 
 def get_sum(x, y):
-    return x + y
+    # return x + y
+    return True, {"data": x+y}
 
 
 def get_square(x):
-    return x ** 2
+    # return x ** 2
+    return True, {"data": x ** 2}
